bb8c648461c871ac02664459b1eab00de75a256e
Maria Camila <camilaalvarez7@hotmail.com>
2016-01-28 09:57:21 -0500

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Maria Camila on 28/01/16.
+//  Copyright © 2016 mindz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
